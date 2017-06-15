d341e7347da772d9a1039e8bd137f8175de69103
Eoghan O'Loughlin <eoghanoloughlin@ymail.com>
2017-06-15 17:57:38 +0100

    Finished example page

--- example/main.js
+++ example/main.js
@@ -1,6 +1,8 @@
 /**
+ * Adds a zero before a number if it's less than 10
+ *
  * @param  {Number}
- * @return {String}
+ * @return {Number|String}
  */
 function prependZero(time) {
 	if (time < 10) {
@@ -12,44 +14,30 @@ function prependZero(time) {
 
 // Once jQuery is loaded
 $(function() {
+	// Keep a record of how much time has passed
+	// since page load
 	var time = 0;
 
-	var $timer = $('.timer');
-
-	// Update the timer on the page to show how long
-	// has passed
+	// Update the timer every second
 	setInterval(function () {
 		time++;
 
-		$timer.text(prependZero(time));
+		$('.timer').text(prependZero(time));
 	}, 1000);
 
-	// The dom element we are waiting to be in the viewport
+	// Loop through each text element
 	$(".text").each(function(i, element) {
 		var $el = $(element);
 
+		// When it becomes visible, show the current time since page load
 		$el.on("visible", function () {
-			console.log(time);
+			$el.text("You scrolled to this point in "+time+" seconds");
 		});
 
-		wait($el);
+		// Bind the event listener, specifying the event should be
+		// triggered 20px away from the dom element
+		wait($el, {
+			offset: 20
+		});
 	});
-
-
-	/*	
-	$text.on("visible", function (e) {
-		// $(e.target).find(".title").removeClass("hidden");
-
-
-	});
-
-	// Instantiate wait on the element
-	*/
-
-	// wait($pageTwo, {
-	// 	offset:  0,
-	// 	callback: function (e) {
-			
-	// 	}
-	// })
 });
